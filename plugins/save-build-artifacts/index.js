2abaf4d8005cd959e0c9ebfd6fab2102d157c503
Chris Hannaby <chris.hannaby@netlify.com>
2022-05-15 19:43:17 -0600

    rename env vars

--- plugins/save-build-artifacts/index.js
+++ plugins/save-build-artifacts/index.js
@@ -4,12 +4,12 @@ var AWS = require("aws-sdk");
 
 function uploadToS3(fileName) {
   const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
   });
   const fileContent = fs.readFileSync(fileName);
   const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
+    Bucket: process.env.MY_AWS_BUCKET_NAME,
     Key: fileName,
     Body: fileContent,
   };
