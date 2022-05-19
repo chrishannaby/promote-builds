82ce367895383a1f2cc507ae2e3f160374ad2480
Chris Hannaby <chris.hannaby@netlify.com>
2022-05-19 17:08:35 -0600

    return sites from api

--- api/netlify/functions/api.js
+++ api/netlify/functions/api.js
@@ -1,4 +1,19 @@
-exports.handler = async (event, context) => {
+const NetlifyAPI = require("netlify");
+const sites = {
+  "e675dda5-c476-4e6b-850d-6cb6df3a39f3": { name: "Production" },
+  "33f525cd-6f1d-4bf5-8a32-ff20fc1f0c21": {
+    name: "QA",
+    promoteTo: "e675dda5-c476-4e6b-850d-6cb6df3a39f3",
+  },
+  "5ade471d-9800-4baa-9101-7451966efd6b": {
+    name: "Release",
+    promoteTo: "33f525cd-6f1d-4bf5-8a32-ff20fc1f0c21",
+  },
+};
+
+exports.handler = async function (event, context) {
+  const client = new NetlifyAPI(process.env.NETLIFY_API_ACCESS_TOKEN);
+
   const path = event.path.replace(/\.netlify\/functions\/[^/]+/, "");
   const segments = path.split("/").filter((e) => e);
 
@@ -11,10 +26,22 @@ exports.handler = async (event, context) => {
           body: "hello world",
         };
       }
-      /* GET /.netlify/functions/api/123456 */
-      if (segments.length === 1) {
-        event.id = segments[0];
-        return;
+      /* GET /.netlify/functions/api/sites */
+      if (segments.length === 1 && segments[0] === "sites") {
+        const filteredSites = [];
+        for (const site in sites) {
+          const siteData = await client.getSite({ site_id: site });
+          filteredSites.push({
+            ...sites[site],
+            id: site,
+            url: siteData.ssl_url,
+            publishedDeploy: siteData.published_deploy,
+          });
+        }
+        return {
+          statusCode: 200,
+          body: JSON.stringify(filteredSites),
+        };
       } else {
         return {
           statusCode: 500,
